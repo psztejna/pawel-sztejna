841d699dc77461d18a280ce59b4f5fa843131afc
Paweł Sztejna <psztejna@linux-p110-12.inf.ug.edu.pl>
2017-10-22 12:36:47 +0200

    wypisuje wyrazenia

---
+++ wypisuje.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char const *argv[]) {
+
+printf ("2+2 = %d\n", 2+2);
+printf ("2-2 = %d\n", 2-2);
+printf ("2*2 = %d\n", 2*2);
+printf ("2/2 = %d\n", 2/2);
+printf ("2|2 = %d\n", 2|2);
+printf ("2&2 = %d\n", 2&2);
+printf ("2||2 = %d\n", 2||2);
+printf ("2&&2 = %d\n", 2&&2);
+printf ("2>2 = %d\n", 2>2);
+printf ("2<2 = %d\n", 2<2);
+printf ("2>=2 = %d\n", 2=>2);
+printf ("2=<2 = %d\n", 2<=2);
+printf ("2>>2 = %d\n", 2>>2);
+printf ("2<<2 = %d\n", 2<<2);
+printf ("2==2 = %d\n", 2==2);
+
+  return 0;
+}
